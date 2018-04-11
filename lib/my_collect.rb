5debc2fbb33e81e238384b0543070e21cc0dcef1
Ayana Bando <ayanaflax@yahoo.com>
2018-04-11 01:17:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ require 'pry'
 def my_collect(array)
   collection = []
   i = 0
-  while i < languages.length
+  while i < array.length
     collection << yield(languages[i]).upcase
     i += 1
     binding.pry
