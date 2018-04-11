ac2db8a03801c120cd72059c5428c1c0dc912e3d
Ayana Bando <ayanaflax@yahoo.com>
2018-04-11 01:14:32 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(languages)
   i = 0
   collection = []
   while i < languages.length
-    collection << yield(languages[i]).uppercase
+    collection << yield(languages[i]).upcase
     binding.pry
     i += 1
   end
